89fbc1c007311a31208c9f1e84cb3ac12138b5ac
Mohit <mynkchauhan1234@gmail.com>
2022-06-30 10:57:46 +0530

    Goals

--- Week4/csrs/pages/index.js
+++ Week4/csrs/pages/index.js
@@ -5,17 +5,17 @@ function Home() {
   return (
     <ul className='style'>
       <li>
-        <Link href="./render/csr">
+        <Link href="./render/csr" prefetch={false}>
           <a>CSR</a>
         </Link>
       </li>
       <li>
-        <Link href="./render/ssr">
+        <Link href="./render/ssr" prefetch={false}>
           <a>SSR</a>
         </Link>
       </li>
       <li>
-        <Link href="./render/ssg">
+        <Link href="./render/ssg" prefetch={false}>
           <a>SSG</a>
         </Link>
       </li>
